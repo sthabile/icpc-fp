32d6434f06ba31adc6abd470beae01a1935bbd6b
Sthabile Lushaba <G17L3794@ict.ru.ac.za>
2019-04-13 18:13:56 +0200

    Fix commaSprinkler

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -2,6 +2,18 @@
 open System
 
 let commaSprinkler (input:string) = 
+    let IsText (text:string) = 
+        match text.Length>=1 with 
+        |true -> 
+            let rec checkchar (txt:string) idx =
+                match idx< txt.Length ,(Char.IsLetter(txt.[idx])||txt.[idx]='.'||txt.[idx]=',') with
+                |true,true -> checkchar txt (idx+1)
+                |true,false -> false
+                |false,_-> true
+            match checkchar text 0 with
+            |true -> true
+            |_ -> false
+        |_ -> false
     let text = string input 
     let InputArray = text.Split()
  
@@ -25,21 +37,16 @@ let commaSprinkler (input:string) =
     //look for repeated words and addcomma 
     let rec FindAndAdd (list:string list) word x =
         match list with 
-        |[] -> "" //not sure what to return yet
+        |[] -> list //not sure what to return yet
         |curr::tail ->
-            match curr.EndsWith('.'), curr=word  with
-            |false,true -> AddComma curr x
+            match curr.EndsWith('.') ,curr=word  with
+            |false,true -> AddComma curr x::tail
             |false,_ -> FindAndAdd tail word x
-            |true,_ ->curr //if the next matching word is followed by a full stop then
+            |true,_ -> tail//if the next matching word is followed by a full stop then
 
-    let IsText (text:string) =
-        match text.Length=1,text.EndsWith('.') with
-        |true ->
-            true
-        |_ -> false
     //something like for int i=0;i<words.length;i++
     let rec f words  acc=
-        match words with
+        match VerifiedText with
         |[]-> None   //base case
         |curr::tail->  // the idea is to compare curr with everything in tail
             match curr.ToString().StartsWith(','),curr with 
@@ -57,23 +64,13 @@ let commaSprinkler (input:string) =
    // failwith "Not implemented"
     
 let rivers input =
-    let list = string input
-    let Array = list.Split(" ")
-    let rec MySplitter (words:string []) acc =  //function for spliting text into array
-                 // should return an list (char list )
-            let wordslength = words.Length
-            match acc<wordslength with
-            |false -> []
-            |true -> 
-                match acc<wordslength,words.[acc] with
-                | true,x ->x::MySplitter words (acc+1)
-                |_ -> []
-            
-    let ans =MySplitter Array 0
-    ans
-   
+    failwith "not impplemented"
 
+    
+
+    failwith "not implemneted"
 [<EntryPoint>]
 let main argv =
+    
     printfn "Hello World from F#!"
     0 // return an integer exit code
